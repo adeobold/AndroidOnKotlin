a5fd0c2709cfa8865d52f45a1016020179a8bf4d
adeobold <adeobold@gmail.com>
2022-06-22 14:29:31 +0300

    Урок 1 / Сделал кнопку и нажатие на нее

--- app/src/main/java/com/android1/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/android1/androidonkotlin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.android1.androidonkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnHello).setOnClickListener{
+            Log.d("$$$", "Нажали на кнопку!")
+        }
+
     }
 }
