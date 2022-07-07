fb2e43883f39ded6b386b79499e0c69ff12353f3
adeobold <adeobold@gmail.com>
2022-07-07 15:15:13 +0300

    Урок 5 / Вынес загрузку в WeatherLoader.kt с коллбэком

---
+++ app/src/main/java/com/android1/androidonkotlin/utils/WeatherLoader.kt
@@ -0,0 +1,24 @@
+package com.android1.androidonkotlin.utils
+
+import com.android1.androidonkotlin.model.dto.WeatherDTO
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+object WeatherLoader {
+    fun request(lat: Double, lon: Double, block: (weather: WeatherDTO) -> Unit) {
+        val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}")
+        val myConnection: HttpURLConnection?
+
+        myConnection = uri.openConnection() as HttpURLConnection
+        myConnection.readTimeout = 5000
+        myConnection.addRequestProperty("X-Yandex-API-Key", "..")
+        Thread {
+            val reader = BufferedReader(InputStreamReader(myConnection.inputStream))
+            val weatherDTO = Gson().fromJson(getLines(reader), WeatherDTO::class.java)
+            block(weatherDTO)
+        }.start()
+    }
+}
--- app/src/main/java/com/android1/androidonkotlin/view/details/DetailsFragment.kt
+++ app/src/main/java/com/android1/androidonkotlin/view/details/DetailsFragment.kt
@@ -9,12 +9,7 @@ import androidx.fragment.app.Fragment
 import com.android1.androidonkotlin.databinding.FragmentDetailsBinding
 import com.android1.androidonkotlin.domain.WeatherItem
 import com.android1.androidonkotlin.model.dto.WeatherDTO
-import com.android1.androidonkotlin.utils.getLines
-import com.google.gson.Gson
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.URL
-import javax.net.ssl.HttpsURLConnection
+import com.android1.androidonkotlin.utils.WeatherLoader
 
 class DetailsFragment : Fragment() {
 
@@ -46,45 +41,28 @@ class DetailsFragment : Fragment() {
         val weatherItem =
             arguments?.getParcelable<WeatherItem>(BUNDLE_WEATHER_EXTRA)
 
-        weatherItem?.let {
-
-            val uri = URL("https://api.weather.yandex.ru/v2/informers?lat=${it.city?.lat}&lon=${it.city?.lon}")
-            val connection: HttpsURLConnection?
-            connection = uri.openConnection() as HttpsURLConnection
-            connection.connectTimeout = 5000
-            connection.addRequestProperty("X-Yandex-API-Key","..")
-
-            Thread{
-                val reader = BufferedReader(InputStreamReader(connection.inputStream))
-                val weatherDTO = Gson().fromJson(getLines(reader),WeatherDTO::class.java)
-
-                requireActivity().runOnUiThread{
-                    renderData(it.apply {
-                        temperature = weatherDTO.fact.temp
-                        humidity = weatherDTO.fact.humidity
-                        pressure = weatherDTO.fact.pressureMm
-                        wind = weatherDTO.fact.windSpeed
-                    })
+        weatherItem?.let { weatherLocal ->
+            weatherLocal.city?.let { cityLocal ->
+                WeatherLoader.request(cityLocal.lat, cityLocal.lon) { weatherDTO ->
+                    bindWeatherLocalWithWeatherDTO(weatherLocal, weatherDTO)
                 }
-
-            }.start()
-
-
-
+            }
         }
 
+    }
 
-
-
-
-
-
-
-//        arguments?.let {
-//            arg -> arg.getParcelable<WeatherItem>(BUNDLE_WEATHER_EXTRA)?.let {
-//                weather -> renderData(weather)
-//            }
-//        }
+    private fun bindWeatherLocalWithWeatherDTO(
+        weatherLocal: WeatherItem,
+        weatherDTO: WeatherDTO
+    ) {
+        requireActivity().runOnUiThread {
+            renderData(weatherLocal.apply {
+                weatherLocal.temperature = weatherDTO.fact.temp
+                weatherLocal.humidity = weatherDTO.fact.humidity
+                weatherLocal.pressure = weatherDTO.fact.pressureMm
+                weatherLocal.wind = weatherDTO.fact.windSpeed
+            })
+        }
     }
 
     @SuppressLint("SetTextI18n")
