2003047c0c605faa0c146622279e4fbcf6a84f43
adeobold <adeobold@gmail.com>
2022-07-25 11:44:35 +0300

    Урок 10 / Добавлен фрагмент для поиска города

--- app/src/main/java/com/android1/androidonkotlin/view/maps/MapsFragment.kt
+++ app/src/main/java/com/android1/androidonkotlin/view/maps/MapsFragment.kt
@@ -1,5 +1,7 @@
 package com.android1.androidonkotlin.view.maps
 
+import android.graphics.Color
+import android.location.Geocoder
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -7,42 +9,108 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android1.androidonkotlin.R
+import com.android1.androidonkotlin.databinding.FragmentMapsBinding
+import com.android1.androidonkotlin.databinding.FragmentMapsUiBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class MapsFragment : Fragment() {
 
+    lateinit var map: GoogleMap
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
+        map = googleMap
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
+        googleMap.setOnMapLongClickListener { latLng ->
+            addMarkerToArray(latLng)
+            setMarker(latLng, "", R.drawable.ic_map_marker)
+            drawLine()
+        }
+
+
+        googleMap.uiSettings.isZoomControlsEnabled = true
+
+        // TODO HW найти подвох
+        googleMap.isMyLocationEnabled = true
+        googleMap.uiSettings.isMyLocationButtonEnabled = true
     }
+
+
+    private var _binding: FragmentMapsUiBinding? = null
+    private val binding: FragmentMapsUiBinding
+        get() {
+            return _binding!!
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        _binding = FragmentMapsUiBinding.inflate(layoutInflater)
+        return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        binding.buttonSearch.setOnClickListener {
+            binding.searchAddress.text.toString().let { searchText ->
+                val geocoder = Geocoder(requireContext())
+                val result = geocoder.getFromLocationName(searchText, 1)
+                // TODO HW а не пустой ли result
+                val ln = LatLng(result.first().latitude, result.first().longitude)
+                setMarker(ln, searchText, R.drawable.ic_map_marker)
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(ln, 15f))
+
+
+            }
+        }
     }
+
+    val markers = mutableListOf<Marker>()
+    private fun addMarkerToArray(location: LatLng) {
+        val marker = setMarker(location, markers.size.toString(), R.drawable.ic_map_pin)
+        markers.add(marker)
+    }
+
+
+    private fun drawLine() {
+        val last: Int = markers.size - 1
+        if (last >= 1) {
+            val previous: LatLng = markers[last - 1].position
+            val current: LatLng = markers[last].position
+            map.addPolyline(
+                PolylineOptions()
+                    .add(previous, current)
+                    .color(Color.RED)
+                    .width(15f)
+            )
+        }
+    }
+
+
+    private fun setMarker(
+        location: LatLng,
+        searchText: String,
+        resourceId: Int
+    ): Marker {
+        return map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+                .icon(BitmapDescriptorFactory.fromResource(resourceId))
+        )!!
+    }
+
 }
